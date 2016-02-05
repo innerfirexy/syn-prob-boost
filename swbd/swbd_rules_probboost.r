1b3a7d2b12480949fc240eeb0fd9654927ff2c70
Yang Xu <innerfirexy@gmail.com>
2016-02-04 21:13:06 -0500

    done swbd_rules_probboost.r

--- swbd/swbd_rules_probboost.r
+++ swbd/swbd_rules_probboost.r
@@ -33,4 +33,54 @@ dt.db.agg = readRDS('dt.db.agg.rds')
 # read df.rulefreq.rds
 df.rf = readRDS('df.rulefreq.rds')
 
+# read df.ext.rds
+df.ext = readRDS('df.ext.rds')
+dt.ext = data.table(df.ext)
+setkey(dt.ext, convID, primeTurnID, targetTurnID)
+
 # define high and low freq ruleIDs
+rule_ids_high = df.rf[1:27,]$ruleID
+rule_ids_low = df.rf[28:100,]$ruleID
+
+# compute probBoost for distance from 1 to 10, and for each type (self, other)
+system.time(pb_other_hi1 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 1), dt.db.agg, rule_ids_high)) # takes 9.5 sec
+pb_other_hi3 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 3), dt.db.agg, rule_ids_high)
+pb_other_hi5 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 5), dt.db.agg, rule_ids_high)
+pb_other_hi7 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 7), dt.db.agg, rule_ids_high)
+pb_other_hi9 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 9), dt.db.agg, rule_ids_high)
+
+pb_other_lo1 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 1), dt.db.agg, rule_ids_low)
+pb_other_lo3 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 3), dt.db.agg, rule_ids_low)
+pb_other_lo5 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 5), dt.db.agg, rule_ids_low)
+pb_other_lo7 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 7), dt.db.agg, rule_ids_low)
+pb_other_lo9 <- ruleProbBoost(subset(dt.ext, type == 'other' & distance == 9), dt.db.agg, rule_ids_low)
+
+pb_self_hi2 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 2), dt.db.agg, rule_ids_high)
+pb_self_hi4 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 4), dt.db.agg, rule_ids_high)
+pb_self_hi6 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 6), dt.db.agg, rule_ids_high)
+pb_self_hi8 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 8), dt.db.agg, rule_ids_high)
+pb_self_hi10 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 10), dt.db.agg, rule_ids_high)
+
+pb_self_lo2 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 2), dt.db.agg, rule_ids_low)
+pb_self_lo4 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 4), dt.db.agg, rule_ids_low)
+pb_self_lo6 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 6), dt.db.agg, rule_ids_low)
+pb_self_lo8 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 8), dt.db.agg, rule_ids_low)
+pb_self_lo10 <- ruleProbBoost(subset(dt.ext, type == 'self' & distance == 10), dt.db.agg, rule_ids_low)
+
+# construct df.plot
+df.plot = data.frame(probBoost = c(pb_other_hi1, pb_other_hi3, pb_other_hi5, pb_other_hi7, pb_other_hi9,
+    pb_other_lo1, pb_other_lo3, pb_other_lo5, pb_other_lo7, pb_other_lo9,
+    pb_self_hi2, pb_self_hi4, pb_self_hi6, pb_self_hi8, pb_self_hi10,
+    pb_self_lo2, pb_self_lo4, pb_self_lo6, pb_self_lo8, pb_self_lo10),
+    distance = c(rep(rep(c(1,3,5,7,9), each = length(rule_ids_high)), 2),
+                rep(rep(c(2,4,6,8,10), each = length(rule_ids_low)), 2)),
+    freq = rep(c(rep('high', 5*length(rule_ids_high)), rep('low', 5*length(rule_ids_low))), 2),
+    type = c(rep('other', 5*length(rule_ids_high) + 5*length(rule_ids_low)),
+            rep('self', 5*length(rule_ids_high) + 5*length(rule_ids_low)))
+)
+
+# remove infinite values
+df.plot[is.infinite(df.plot$probBoost),]$probBoost = NA
+
+# save df.plot to rds
+saveRDS(df.plot, 'df.plot.rds')
