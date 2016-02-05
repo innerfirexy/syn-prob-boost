d12e3baa4e1e24e9f98ee51815290e82ce4a0ce5
Yang Xu <innerfirexy@gmail.com>
2016-02-04 19:40:58 -0500

    modify addInLabel and add ruleProbBoost funcs in swbd_func.r

--- swbd/swbd_func.r
+++ swbd/swbd_func.r
@@ -31,7 +31,7 @@ addInLabel = function(df.pairs, dt.db.agg, rule_id) {
     dt.db.agg = dt.db.agg[, {
             rs = unlist(strsplit(rules, ','));
             r = as.character(rule_id);
-            list(xmlID = xmlID, divIndex = divIndex, turnID = turnID, label = r %in% rs)
+            list(convID = convID, turnID = turnID, label = r %in% rs)
         }, by = 1:nrow(dt.db.agg)]
     dt.db.agg[, nrow:=NULL]
     setkey(dt.db.agg, convID, turnID)
@@ -48,18 +48,30 @@ addInLabel = function(df.pairs, dt.db.agg, rule_id) {
     unique_turns = unique(rbindlist(list(prime_turns, target_turns)))
     setkey(unique_turns, convID, turnID)
 
-    # merge from dt.db.agg to unique_turns
-    unique_turns = merge(unique_turns, dt.db.agg)
+    # join from dt.db.agg to unique_turns
+    unique_turns = unique_turns[dt.db.agg, nomatch = 0]
 
-    # rename and merge back to dt.pairs
+    # join and back to dt.pairs
     unique_turns = rename(unique_turns, primeTurnID = turnID)
-    dt.pairs = merge(dt.pairs, unique_turns)
+    dt.pairs = dt.pairs[unique_turns, nomatch = 0]
     dt.pairs = rename(dt.pairs, inPrime = label)
 
     setkey(dt.pairs, convID, targetTurnID)
     unique_turns = rename(unique_turns, targetTurnID = primeTurnID)
-    dt.pairs = merge(dt.pairs, unique_turns)
+    dt.pairs = dt.pairs[unique_turns, nomatch = 0]
     dt.pairs = rename(dt.pairs, inTarget = label)
     # return
     dt.pairs
+}
+
+# the func that computes the probability boost for a single or a series of rule IDs
+ruleProbBoost = function(dt.pairs, dt.db.agg, rule_ids) {
+    registerDoMC(detectCores())
+    res = foreach(rule_id = rule_ids, .combine = c) %dopar% {
+        dt.labeled = addInLabel(dt.pairs, dt.db.agg, rule_id)
+        prior = nrow(subset(dt.labeled, inTarget)) / nrow(dt.labeled)
+        cond = nrow(subset(dt.labeled, inTarget & inPrime)) / nrow(subset(dt.labeled, inPrime))
+        log(cond) - log(prior) # log odds ratio
+    }
+    res
 }
