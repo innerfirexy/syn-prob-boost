f5f53144021ee7dd124a2c27e5ecba2f3057c0db
Yang Xu <innerfirexy@gmail.com>
2016-02-04 19:26:54 -0500

    save dt.db.agg.rds

--- swbd/swbd_rules_probboost.r
+++ swbd/swbd_rules_probboost.r
@@ -12,14 +12,25 @@ library(ggplot2)
 # include swbd_func.r
 source('swbd_func.r')
 
-# read df.db from entropy table of swbd db
-# ssh yvx5085@brain.ist.psu.edu -i ~/.ssh/id_rsa -L 1234:localhost:3306
-conn = dbConnect(MySQL(), host = '127.0.0.1', user = 'yang', port = 1234, password = "05012014", dbname = 'swbd')
-sql = 'SELECT convID, turnID, subRulesID FROM entropy'
-df.db = dbGetQuery(conn, sql)
+# # read df.db from entropy table of swbd db
+# # ssh yvx5085@brain.ist.psu.edu -i ~/.ssh/id_rsa -L 1234:localhost:3306
+# conn = dbConnect(MySQL(), host = '127.0.0.1', user = 'yang', port = 1234, password = "05012014", dbname = 'swbd')
+# sql = 'SELECT convID, turnID, subRulesID FROM entropy'
+# df.db = dbGetQuery(conn, sql)
+#
+# # compute dt.db.agg
+# dt.db = data.table(df.db)
+# setkey(dt.db, convID, turnID)
+# system.time(dt.db.agg <- dt.db[, .(rules = dbRulesAgg(subRulesID)), by = .(convID, turnID)])
+# # takes 5 sec
+#
+# # save dt.db.agg to rds
+# saveRDS(dt.db.agg, 'dt.db.agg.rds')
 
-# compute dt.db.agg
-dt.db = data.table(df.db)
-setkey(dt.db, convID, turnID)
-system.time(dt.db.agg <- dt.db[, .(rules = dbRulesAgg(subRulesID)), by = .(convID, turnID)])
-# takes 5 sec
+# read dt.db.agg.rds
+dt.db.agg = readRDS('dt.db.agg.rds')
+
+# read df.rulefreq.rds
+df.rf = readRDS('df.rulefreq.rds')
+
+# define high and low freq ruleIDs
